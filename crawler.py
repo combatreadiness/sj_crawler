578222d167a86d9302e6121b24e5f98a53d148a2
combatreadiness <swleeyg@gmail.com>
2020-03-17 02:51:31 -0700

    new

--- crawler.py
+++ crawler.py
@@ -1,16 +1,22 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
+import asyncio
 
+class Crawler:
+    def __init__(self):
+        with open('site_info.json') as json_file:
+            self.json_data = json.load(json_file)
 
-def findMyNews(press_name: "cnn", keywords: []) -> {}:
-    with open('site_info.json') as json_file:
-        json_data = json.load(json_file)
+    def getListOfPress(self):
+        return list(self.json_data.keys())
+
+   def findMyNews(self, press_name: "cnn_us", keywords: []) -> {}:
         try:
-            website = json_data[press_name]["url"]
-            link_url = json_data[press_name]["link_url"]
-            title_tag = json_data[press_name]["title_tag"]
-            title_class = json_data[press_name]["title_class"]
+            website = self.json_data[press_name]["url"]
+            link_url = self.json_data[press_name]["link_url"]
+            title_tag = self.json_data[press_name]["title_tag"]
+            title_class = self.json_data[press_name]["title_class"]
 
         except KeyError:
             print("I can't find your input in the Press List.")
@@ -20,16 +26,19 @@ def findMyNews(press_name: "cnn", keywords: []) -> {}:
         html = urlopen(website)
         bsObject = BeautifulSoup(html, "lxml")
         results = bsObject.findAll(title_tag, class_=title_class)
-        #print(results)
+        # print(results)
         for r in results:
             title = r.text
             for keyword in keywords:
                 if keyword in title:
-                    my_news_list[title] = [press_name,link_url + r.find('a').get('href')]
+                    my_news_list[title] = [press_name, link_url + r.find('a').get('href')]
                     break
-    return my_news_list
 
+        return my_news_list
 
-final_results=(findMyNews('fox', ["Trump"]))
-for k,v in final_results.items():
-    print(v[0],":",k)
+if __name__ == '__main__':
+    c = crawler()
+    final_results = (c.findMyNews('fox', ["Trump"]))
+    c.getListOfPress()
+    for k, v in final_results.items():
+        print(v[0], ":", k)
--- kivy_app.py
+++ kivy_app.py
@@ -1,16 +1,73 @@
-import kivy
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from crawler import Crawler
+from kivymd.app import MDApp
+from kivymd.uix.chip import MDChip
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.list import TwoLineListItem
+import asyncio
 
-kivy.require('1.11.1')
 
-
-class inputLayout(GridLayout):
+class SearchPage(Screen):
     pass
 
-class KivyApp(App):
-    def build(self):
-        return inputLayout()
+
+class ResultPage(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class KivyApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cr = Crawler()
+        self.search_list = []
+
+    def on_start(self):
+        self.showPressList()
+
+    def showPressList(self):
+        press_list = self.cr.getListOfPress()
+        for name in press_list:
+            self.root.ids.SearchPage.ids.pressList.add_widget(
+                MDChip(
+                    label=f"{name}",
+                    icon=f"alpha-{name.lower()[0]}-circle-outline",
+                    check="True",
+                    callback=self.selectPress,
+                    color=[0, 0, 0, 0.3]
+                )
+            )
+
+    def selectPress(self, name_chip, name):
+        if len(name_chip.ids.box_check.children):
+            self.search_list.append(name)
+            name_chip.color = self.theme_cls.primary_color
+        else:
+            self.search_list.remove(name)
+            name_chip.color = [0, 0, 0, 0.3]
+        print(self.search_list)
+        pass
+
+    def findNews(self):
+        print("test")
+        loop = asyncio.get_event_loop()  # 이벤트 루프를 얻음
+        loop.run_until_complete(self.getNews())  # main이 끝날 때까지 기다림
+        loop.close()
+
+    async def getNews(self):
+        final_list = {}
+        keywords = (self.root.ids.SearchPage.ids.textField.text.replace(" ", "").split(','))
+        print(keywords)
+        futures = [asyncio.ensure_future(self.cr.findMyNews(press, keywords)) for press in self.search_list]
+        result = await asyncio.gather(*futures)
+        print(result)
+        #for press in self.search_list:
+        #    final_list.update(self.cr.findMyNews(press, keywords))
+        #for k, v in final_list.items():
+        #    print(v[0], ":", k)
+
 
 if __name__ == '__main__':
     KivyApp().run()
