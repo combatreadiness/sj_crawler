7130ece742099c26c0c516c23747a1f0df79884d
combatreadiness <swleeyg@gmail.com>
2020-03-20 00:02:50 -0700

    new

--- kivy_app.py
+++ kivy_app.py
@@ -1,5 +1,7 @@
+import asyncio
 import logging
 import threading
+import time
 
 from crawler import Crawler
 import webbrowser
@@ -11,7 +13,7 @@ from kivy.metrics import dp
 
 from kivymd.app import MDApp
 from kivymd.uix.chip import MDChip
-from kivymd.uix.list import TwoLineListItem
+from kivymd.uix.list import TwoLineAvatarIconListItem
 from kivymd.uix.snackbar import Snackbar
 
 from queue import Queue
@@ -32,17 +34,15 @@ class WindowManager(ScreenManager):
 class KivyApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.stop_event = threading.Event()
         self.search_list = []
         self.keywords = []
         self.cr = Crawler()
         self.news_list = {}
         self.uploaded_news_list = {}
         self.news_queue = Queue()
-        self.snackbar = None
-        self._interval = 0
-        self.crawlerThread = None
-
-
+        self.sb = None
+        self.news_event = None
 
     def on_start(self):
         self.showPressList()
@@ -69,39 +69,31 @@ class KivyApp(MDApp):
             name_chip.color = [0, 0, 0, 0.3]
         pass
 
-    # def findNews(self):  #start Second Thread
-    #    Thread(target=self.showNewsListThread).start()
-
     def runResultPage(self):
-        print("here111")
         self.keywords = (self.root.ids.SearchPage.ids.textField.text.replace(" ", "").split(','))
         if len(self.keywords[0]) == 0:
-            print("no keywords")
             self.snackbar_show("Please Type Keywords")
             return
         if len(self.search_list) == 0:
-            print("no press selected")
             self.snackbar_show("Please Select at least one press")
             return
-
+        print(self.keywords, " and ",self.search_list)
         self.root.ids.WM.current = "result"
         self.showNewsListThread()
         self.getNewsListThread()
 
-
-
     def snackbar_show(self, msg):
-        if not self.snackbar:
-            self.snackbar = Snackbar(text=msg)
-            self.snackbar.show()
-            anim = Animation(y= dp(72), d=.2)
-            self.snackbar = None
-
-
+        if not self.sb:
+            self.sb = Snackbar(text=msg)
+            self.sb.show()
+            anim = Animation(y=dp(72), d=.2)
+            self.sb = None
 
     def showNewsListThread(self):
+        self.uploaded_news_list = {}
+        self.root.ids.ResultPage.ids.NewsList.clear_widgets()
         Clock.schedule_once(self.showNewsList, 0)
-        Clock.schedule_interval(self.showNewsList, 5)
+        self.news_event = Clock.schedule_interval(self.showNewsList, 5)
 
     def showNewsList(self, dt):
         while self.news_queue.qsize():
@@ -109,26 +101,29 @@ class KivyApp(MDApp):
             for k, v in result.items():
                 if k not in self.uploaded_news_list.keys():
                     self.root.ids.ResultPage.ids.NewsList.add_widget(
-                        TwoLineListItem(
+                        TwoLineAvatarIconListItem(
                             text=k,
                             secondary_text=v[0]
                         )
                     )
                     self.uploaded_news_list[k] = [v[0]]
 
-
-
-
     def getNewsListThread(self):
-        self.crawlerThread = threading.Timer(0, self.getNews)
-        self.crawlerThread.start()
-
-    def getNews(self):
         for press in self.search_list:
+            t = threading.Thread(target=self.getNews, args=(press,5))
+            t.daemon = True
+            t.start()
+
+    def getNews(self, press, seconds):
+        while True:
             self.news_queue.put(self.cr.findMyNews(press, self.keywords))
-        self.crawlerThread = threading.Timer(10, self.getNews)
-        self.crawlerThread.daemon = True
-        self.crawlerThread.start()
+            if self.stop_event.isSet():
+                break
+            time.sleep(seconds)
+
+    def clearThreads(self):
+        self.stop_event.set()
+        self.news_event.cancel()
 
     def on_stop(self):
         print("Exit")
